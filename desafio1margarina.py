3456ad4dd7b985705922d154ba39f7db4aae8943
Lichaprogramacion <Lisandrolucasramos@gmail.com>
2024-05-07 14:54:25 -0300

    Update desafio1margarina.py

--- desafio1margarina.py
+++ desafio1margarina.py
@@ -1,24 +1,24 @@
 import random
 tiempo = 0
-jargentina= [('Agustina Gorzelany',11),
-             ('Maria Jose Granatto',9),
-             ('Sofia Toccalino',20),
-             ('Agostina Alonso',10),
-             ('Valentina Raposo',8),
-             ('Clara Barberi',5),
-             ('Delfina Thome',4),
-             ('Sofia Cairó',7),
-             ('Pilar Campoy',16),
+jargentina= [(11,'Agustina Gorzelany'),
+             (9,'Maria Jose Granatto'),
+             (20,'Sofia Toccalino'),
+             (10,'Agostina Alonso'),
+             (8,'Valentina Raposo'),
+             (5,'Clara Barberi'),
+             (4,'Delfina Thome'),
+             (7,'Sofia Cairó'),
+             (16,'Pilar Campoy'),
              ]
-jaustralia= [('Madonna Blyth',9),
-             ('Toni Cronk',8),
-             ('',20),
-             ('Katrina Powell',10),
-             ('',8),
-             ('',5),
-             ('',4),
-             ('',7),
-             ('',16),
+jaustralia= [(9,'Madonna Blyth'),
+             (8,'Toni Cronk'),
+             (20,'Alice Arnot'),
+             (10,'Katrina Powell'),
+             (1,'Kobie Mcgurk'),
+             (5,'Jocelyn Bartman'),
+             (4,'Maddison Brooks'),
+             (7,'Jane Claxton'),
+             (16,'Claire colwill'),
             ]
 def pase():
         if random.randint(0,1)==1:
@@ -26,11 +26,11 @@ def pase():
         else:
             print('el pase no fue exitoso')
 def paseargentina(tiempo):
-    print('La jugadora ',jargentina[random.randint(0,8)],' hizo un pase en el minuto ',tiempo,end=' ')
+    print('La jugadora',jargentina[random.randint(0,8)],'hizo un pase en el minuto',tiempo,end=' ')
 def paseaustralia(tiempo):
-    print('La jugadora ',jaustralia[random.randint(0,8)],' hizo un pase en el minuto ',tiempo,end=' ')
+    print('La jugadora',jaustralia[random.randint(0,8)],'hizo un pase en el minuto',tiempo,end=' ')
 while tiempo<60:
-    tiempo = random.randint(tiempo,60)
+    tiempo = random.randint(tiempo,tiempo+1)
     if random.randint(0,1)==0:
         jugadora=paseaustralia(tiempo)
         pase()
