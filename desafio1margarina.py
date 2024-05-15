2eb0fa8254878e8297f56e253d5fa555899beb2d
Lichaprogramacion <Lisandrolucasramos@gmail.com>
2024-05-15 11:09:32 -0300

    Update desafio1margarina.py

--- desafio1margarina.py
+++ desafio1margarina.py
@@ -1,6 +1,5 @@
 import random
-tiempo = 0
-jargentina= [(1,'Agustina Gorzelany'),
+jargentina = [(1,'Agustina Gorzelany'),
              (2,'Maria Jose Granatto'),
              (3,'Sofia Toccalino'),
              (4,'Agostina Alonso'),
@@ -10,9 +9,8 @@ jargentina= [(1,'Agustina Gorzelany'),
              (8,'Sofia Cairó'),
              (9,'Pilar Campoy'),
              (10,'Agustina Albetario'),
-             (11,'Maria Pilar Campoy'),
-             ]
-jaustralia= [(1,'Madonna Blyth'),
+             (11,'Maria Pilar Campoy'),]
+jaustralia = [(1,'Madonna Blyth'),
              (2,'Toni Cronk'),
              (3,'Alice Arnot'),
              (4,'Katrina Powell'),
@@ -20,29 +18,24 @@ jaustralia= [(1,'Madonna Blyth'),
              (6,'Jocelyn Bartman'),
              (7,'Maddison Brooks'),
              (8,'Jane Claxton'),
-             (9,'Claire colwill'),
+             (9,'Claire Colwill'),
              (10,'Penny Squibb'),
-             (11,'Grace Stewart'),
-            ]
-f = open('resultado.txt', 'w')
-def pase():
-        if random.randint(0,1)==1:
-            return 1
-        else:
-            return 0
-def paseargentina(tiempo):
-    escribir=str(('Argentina',jargentina[random.randint(0,8)],pase(),tiempo))
-    f.write(escribir + '\n')
-def paseaustralia(tiempo):
-    escribir=str(('Australia',jaustralia[random.randint(0,8)],pase(),tiempo,))
-    f.write(escribir + '\n')
-while tiempo<60:
-    tiempo = random.randint(tiempo,tiempo+1)
-    if random.randint(0,1)==0:
-        jugadora=paseaustralia(tiempo)
-        pase()
-    else:
-        jugadora=paseargentina(tiempo)
-        pase()
-f.close()
-
+             (11,'Grace Stewart'),]
+f = open('pases.txt', 'w') #Abre o crea el archivo .txt
+ 
+def escribir_resultado(pais, jugador, tiempo, estado_pase): # Escribe en txt (Sobreescribe si es necesario)
+    f.write(f"{pais};{jugador[0]};{jugador[1]};{estado_pase};{tiempo}\n") # Escribe en el archivo .txt con el orden de los datos precisado con un salto de renglón guardado todo en una variable
+    
+def main():
+    tiempo = 0
+    while tiempo<=60: # Se utiliza para que sea dentro del partido de 60 minutos
+        pases = 0
+        while pases<819: # Se necesita esta cantidad para generar los 50000 registros propuestos en promedio
+            if random.randint(0,1)==0: # Determina de manera aleatoria si la jugadora es Australiana, que jugador es el que realiza el pase, si el pase es efectivo y envia los valores hacia la funcion escribir_resultado
+                escribir_resultado("Australia", random.choice(jaustralia) ,tiempo,random.randint(0,1))
+            else: # Determina de manera aleatoria si la jugadora es Argentina, que jugador es el que realiza el pase, si el pase es efectivo y envia los valores hacia la funcion escribir_resultado
+                escribir_resultado("Argentina", random.choice(jargentina) ,tiempo,random.randint(0,1))
+            pases +=1 
+        tiempo +=1
+    f.close() # Cierra el archivo para poder visualizarlo
+main() # Ejecuta el programa principal
